24f35fae4ea293fd11438134585656ff8bd32192
Maria Beatriz Walter Costa <waltercostamb@gmail.com>
2024-03-06 15:55:27 +0100

    added script for iso-elec table

--- scripts/iso-point_table.py
+++ scripts/iso-point_table.py
@@ -1,16 +1,15 @@
 #waltercostamb@gmail.com
 
-#This script converts eggnog emapper's output to a binary CSV table. See wikis: 
-# https://git.bia-christian.de/bia/lab_book_VEO/wiki/pipeline-of-features -> "Gene families"
-# https://git.bia-christian.de/bia/lab_book_VEO/wiki/features -> "Gene functions"-> "Format eggonog output for SVM"
+#This script creates a CSV table of isoelectric points of ortholog proteins of different genomes.
+#See wikis:
+# https://git.bia-christian.de/bia/lab_book_VEO/wiki/pipeline-of-features -> "Isoelectric point of proteins"
 
 #USAGE: (i) activate conda below to use python3 packages in draco and (ii) run the script
-#conda activate bacterial_phenotypes
-#python3 SCRIPT FILE_LIST INPUT_FOLDER OUTPUT_FOLDER
+#conda activate /home/no58rok/tools/miniconda3/envs/bacterial_phenotypes
+#python3 SCRIPT FILE_LIST COG_INPUT_FOLDER OUTPUT_FOLDER ISO_INPUT_FOLDER
 
 #To create FILE_LIST, you can use the command lines below:
-#$ls -lh INPUT_FOLDER/*annotations > pre_file_list.txt
-#$sed 's/  */\t/g' pre_file_list.txt | cut -f 9 | sed 's/\//\t/g' | cut -f3 > file_list.txt
+#$ls -lh INPUT_FOLDER/*annotations | sed 's/  */\t/g' | cut -f 9 | sed 's/\//\t/g' | cut -f3 > file_list.txt
 
 import warnings
 import re
@@ -18,33 +17,70 @@ import time
 from datetime import datetime
 from glob import glob
 import pandas as pd
+import numpy as np
 import sys
+import os
 
 #Check if the expected number of arguments is provided
-if len(sys.argv) < 4:
-    print("Usage: python3 genes_table.py FILE_LIST INPUT_FOLDER OUTPUT_FOLDER")
+if len(sys.argv) < 5:
+    print("Usage: python3 SCRIPT FILE_LIST COG_INPUT_FOLDER OUTPUT_FOLDER ISO_INPUT_FOLDER")
     sys.exit(1)  # Exit the script with a non-zero status indicating error
 
-#Get the second argument given in the command line and store it as input folder
+#Get argument from command line and store it as input folder
 input_folder = sys.argv[2]
 
-#Get the second argument given in the command line and store it as input folder
+#Get argument from command line and store it as output folder
 output_folder = sys.argv[3]
 
 #Open list of filenames (first argument of command line)
 file_of_list_files = sys.argv[1]
 
+#Open argument from command line and store it as input folder contaning isoelectric points 
+input_folder_iso = sys.argv[4]
+
 #Load list of files given in the command line
 list_files = pd.read_csv(f'{file_of_list_files}', header = None, dtype=str)
 list_files = list_files[0].tolist()
 
-cog2presence = {}
-line = 0
+ts = time.time() 
+print("Creating dictionary of protein names and isoelectric points...", datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+
+#Create dictionary of protein names and isoelectric points
+name2point = {}
+
+#Go through list of files
+for filename in list_files:
+
+    #Form the complete path to the file
+    file_path = os.path.join(input_folder_iso, f"{filename.split('/')[0]}-iso_point.csv")
+
+    #Read the CSV file into a DataFrame
+    iso_lines = pd.read_csv(file_path, header=0, sep=';', dtype=str)
+
+    #print(iso_lines)
+
+    for index,row in iso_lines.iterrows():
+        prot_name = row['protein']
+        iso_point = float(row['isoelectric_point'])
+
+        #print(prot_name, iso_point)
+        
+        if filename not in name2point:
+            name2point[filename] = {}
+
+        #Populate dictionary
+        if prot_name not in name2point[filename]:
+            name2point[filename][prot_name] = iso_point
+
+#print(name2point)
 
 ts = time.time() 
 print("Processing files...", datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
 
-#For each file, extract the 5th col. for every line/protein (eggNOG_OGs), create matrix presence/absence of orthologs
+cog2presence = {}
+line = 0
+
+#For each file, extract the 5th col. for every line/protein (eggNOG_OGs), create matrix of orthologs, add isoelectric point of protein to fill the matrix
 for file_name in list_files:
     
     #print(file_name)
@@ -53,7 +89,7 @@ for file_name in list_files:
     #    ts = time.time() 
     #    print("Processing file number", line, datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
     
-    #Open file, store 5th column
+    #Open file, store 5th column (ortholog family name)
     t = input_folder + str(file_name) + '/' + str(file_name) + '.emapper.annotations'
     t2 = glob(t)
     #print(t)
@@ -62,22 +98,27 @@ for file_name in list_files:
 
     #print(file1) 
 
-    #Open file
+    #Open file containing ortholog names
     df = pd.read_csv(file1, sep = '\t', skiprows=5, header = None)
     #Store 5th column with the COGs (ortholog groups)
     #cog_list elements looks like: 
     #'COG4372@1|root,COG4372@2|Bacteria,4PM9B@976|Bacteroidetes,1J0UE@117747|Sphingobacteriia'
     
     cog_full_lst = df[4].tolist()
-    
-    #For each line/protein of the file, process it to store it in the presence/absence matrix
-    for cog_full_el in cog_full_lst:
+    prot_name_lst = df[0]
+
+    #For each line/protein of the file, process it to store isoelectric point matrix
+    for i, cog_full_el in enumerate(cog_full_lst):
+    #for cog_full_el in cog_full_lst:
    
         #print(cog_full_el)
 
         #Avoid end of file lines begining with '#'
         if not isinstance(cog_full_el, float):
         
+            #extract prot_name
+            prot_name = prot_name_lst[i]
+
             #extract the highest level of COG (2nd element)
             cog_lst = cog_full_el.split('|')
         
@@ -87,11 +128,17 @@ for file_name in list_files:
             id_file = file_name
             #print(cog_full_el,id_file)
         
-            #Add info (ortholog's presence) to dictionary -> matrix
-            if(cog in cog2presence):
-                cog2presence[cog][id_file] = 1
+            #Add isoelectric point to variable
+            if (prot_name in name2point[id_file]):    
+                iso_point = name2point[id_file][prot_name]
             else:
-                cog2presence[cog] = {id_file: 1}
+                iso_point = np.nan
+
+            #Add info (isoelectric point) to dictionary -> matrix
+            if(cog in cog2presence):
+                cog2presence[cog][id_file] = iso_point
+            else:
+                cog2presence[cog] = {id_file: iso_point}
                 
     line = line + 1 
     
@@ -104,18 +151,8 @@ df_cog = pd.DataFrame.from_dict(cog2presence, orient='index')
 ts = time.time() 
 print("Data in dataframe", datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
 
-#Substitute NA for zero
-df_cog2 = df_cog.fillna(0)
-
-#Convert data types from float to int
-n = len(df_cog.columns)
-df_cog = df_cog2.iloc[:, :n].astype(int)  
-
-ts = time.time() 
-print("Dataframe NA to 0", datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-
 #Save the dataframe to a CSV file
-df_cog.to_csv(output_folder + 'gene-family_profiles' + '.csv', index=True)
+df_cog.to_csv(output_folder + 'iso-points_profiles' + '.csv', index=True)
 
 ts = time.time() 
-print("Data saved in file", output_folder +"gene-family_profiles.csv", datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+print("Data saved in file", output_folder +"iso-points_profiles.csv", datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
